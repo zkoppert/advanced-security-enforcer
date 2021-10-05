5fad02bb4c6e2170bcbbb5e5d1573b7347c94192
Zack Koppert <zkoppert@github.com>
2021-10-05 14:54:47 -0700

    Initial commit
    Signed-off-by: Zack Koppert <zkoppert@github.com>

---
+++ README.md
@@ -0,0 +1,19 @@
+# Advanced-Security-Enforcer
+
+## What this repo does
+This code is for a GitHub Action to check on a schedule for new repositories and open pull requests in the new repositories for code scanning.
+
+## How it does this
+- A CRON job on github actions triggers a nightly run of this script
+- The script checks for new repositories by storing the known repositories to a file
+- It then iterates over new repositories and opens a pull request for the codeql.yml file stored in this repo
+
+## Instructions to run locally
+- Clone the repository
+- Create a personal access token with read only permissions
+- Copy the `.env-example` file to `.env`
+- Edit the `.env` file by adding your Personal Access Token to it and the desired organization
+- Install dependencies `pip install -r requirements.txt`
+- Run the code `python3 enforcer.py`
+- Note the log output for details on any pull requests that were opened
+- After running locally this will have changed your git config user.name and user.email so those should be reset for this repo
---
+++ enforcer.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+import os
+from datetime import datetime, timedelta
+from os.path import dirname, join
+
+import github3
+from dotenv import load_dotenv
+
+if __name__ == "__main__":
+
+    # Load env variables from file
+    dotenv_path = join(dirname(__file__), ".env")
+    load_dotenv(dotenv_path)
+
+    # Auth and identitiy to GitHub.com
+    gh = github3.login(token=os.getenv("GH_TOKEN"))
+    os.system("git config --global user.name 'Advanced Security Enforcer'")
+    os.system("git config --global user.email 'no-reply@github.com'")
+
+    # Get all repos from organization
+    organization = os.getenv("ORGANIZATION")
+    gh_actor = os.getenv("GH_ACTOR")
+    date = (datetime.now() - timedelta(hours=24)).strftime("%Y-%m-%d")
+    search_string = (
+        "org:" + str(organization) + " created:" + str(date) + " archived:false"
+    )
+    allowed_languages = [
+        "C",
+        "C++",
+        "C#",
+        "Go",
+        "Java",
+        "JavaScript",
+        "TypeScript",
+        "Python",
+    ]
+    all_repos = gh.search_repositories(search_string)
+
+    for short_repository in all_repos:
+        print(short_repository.full_name)
+        # check if the repo is compatible language using short_repository.languages_url
+        for language in short_repository.repository.languages():
+            if language[0] in allowed_languages:
+                # clone the repo
+                os.system(
+                    "git clone https://%s:%s@github.com/%s"
+                    % (gh_actor, os.getenv("GH_TOKEN"), short_repository.full_name)
+                )
+                # checkout a branch called code-scanning
+                os.chdir("%s" % short_repository.name)
+                os.system("git checkout -b code-scanning")
+                # git add the code-scanning file
+                os.system("mkdir -p .github/workflows")
+                # Copy the default configuration file to the proper directory
+                os.system("cp ../codeql.yml .github/workflows/")
+                os.system("git add .github/workflows/codeql.yml")
+                # git commit that file
+                os.system("git commit -m'Request to add code scanning to repository'")
+                # git push -u origin code-scanning
+                os.system("git push -u origin code-scanning")
+                # open a PR from that branch to the default branch
+                default_branch = short_repository.default_branch
+                pr_body = ""
+                short_repository.repository.create_pull(
+                    "Request for Code Scanning",
+                    default_branch,
+                    "code-scanning",
+                    body=pr_body,
+                )
+                # Clean up repository dir
+                os.chdir("../")
+                os.system("rm -rf %s" % short_repository.name)
+                break
