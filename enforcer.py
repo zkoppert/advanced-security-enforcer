045f8c70c14aa466777ec53cf90cf9fa20b5e600
Zack Koppert <zkoppert@github.com>
2022-02-17 14:37:42 -0800

    Convert delay to int before using for time math

--- enforcer.py
+++ enforcer.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     gh_actor = os.getenv("GH_ACTOR")
     hours_delay = 24
     if os.getenv("HOURS_DELAY"):
-        hours_delay = os.getenv("HOURS_DELAY")
+        hours_delay = int(os.getenv("HOURS_DELAY"))
     date = (datetime.now() - timedelta(hours=hours_delay)).strftime("%Y-%m-%d")
     search_string = (
         "org:" + str(organization) + " created:" + str(date) + " archived:false"
